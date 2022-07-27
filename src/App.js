c8f4e4075e63c79f8129544a6bf852890206d999
Siskyy <marcbertelli7@gmail.com>
2022-07-27 16:12:59 +0100

    Header and palette setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// Components
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <Router>
-      
+      <Header />
     </Router>
   )
 }
