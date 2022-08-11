9bc1672c6a53709bc83b1db0bc9b85b4f11914dc
Siskyy <marcbertelli7@gmail.com>
2022-08-11 17:32:02 +0100

    Header navLinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,33 +2,26 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
+import {HiArrowSmRight} from '../../../node_modules/react-icons/hi';
 
 function Header() {
-    return (
-        <header className='p-3 header-box'>
-            <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-        </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="/login" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="/" class="nav-link px-2 text-white">Buy</a></li>
-          <li><a href="/" class="nav-link px-2 text-white">Sell</a></li>
-          <li><a href="/" class="nav-link px-2 text-white">FAQs</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search Stock..." aria-label="Search"/>
-        </form>
-
-        <div class="text-end">
-          <a href="/login"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
-          <a href="/sign-up"><button type="button" class="btn btn-warning">Sign-up</button></a>
-        </div>
-      </div>
-    </div>
+  return (
+        <header className='p-3 header'>
+            <div className="container">
+              <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <nav className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                  <NavLink to='/home' className="nav-link navButton">Home</NavLink>
+                  <NavLink to='/about' className="nav-link navButton">About</NavLink>
+                  <NavLink to='/leagues' className="nav-link navButton">Leagues</NavLink>
+                  <NavLink to='/contact' className="nav-link navButton">Contact us</NavLink>
+                </nav>
+                <div className="text-end">
+                  <button type="button" className="btn me-2 btn-login"><a href="/login" className="no-css">Login</a></button>
+                  <button type="button" className="btn btn-peach"><a href="/sign-up" className="no-css-b">Sign-up <HiArrowSmRight /></a></button>
+                </div>
+              </div>
+            </div>
         </header>
     )
 }
